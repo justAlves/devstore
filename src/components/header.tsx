af817c4887c16638c3301a38d4f4bfe4f23eed57
Guilherme Alves <iamalves.jpg@gmail.com>
2024-02-16 18:14:09 -0300

    api(feat):create api wrapper

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,7 +7,8 @@ export function Header() {
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-5">
         <Link href="/" className="text-2xl font-extrabold text-white">
-          devstore
+          {'<'}dev<span className="text-violet-500">store</span>
+          {'/>'}
         </Link>
 
         <form className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
